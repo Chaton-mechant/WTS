0783d85110e3ebf79f0080333d190747bba0f2b3
Delphin Blehoussi <delphin.blehoussi@student.uclouvain.be>
2022-05-16 16:41:21 +0200

    Renouvellement du fichier exemple

--- src/wts/exemple.py
+++ src/wts/exemple.py
@@ -1,32 +1,47 @@
 #!/usr/bin/env python
 
-
 import sqlite3
 
-
-def wts(data_file:str, sqlite_file:str):
+class Wts:
     """
-    @pre: - Enter the sql file name wich contains all the sql 
-            instructions : `data_file` and the sqlite file name : `sqlite_file`
-    @post: - The sqlite file is created and filled with the data from the
+    WTS class
+    This class is used to manage sqlite database
+    The main purpose of this class is to execute sql commands
     """
-    # Access to the sqlite file
-    conn = sqlite3.connect(sqlite_file) 
 
-    # the cursor is used to execute the sql instructions
-    cursor = conn.cursor()
+    def __init__(self, sqlite_path: str):
+        """
+        Initialize the connection to the sqlite db and create the cursor
+        """
+        self.sqlite_path = sqlite_path
+        self.conn = sqlite3.connect(self.sqlite_path)
+        self.cursor = self.conn.cursor()
 
-    with open (data_file,'r') as dataf: 
+    def __del__(self):
+        """
+        Stop the connection to the sqlite db
+        """
+        self.conn.close()
 
-        for lines in dataf:
-            the_insert = lines.split(";")   
-            cursor.execute('''{}'''.format(the_insert[0]))
+    ################################################################################
+    # The function that read and execute sql command from sql file in the sqlite db
+    # @param sql_file_path: the path of the sql file
+    # @return: None
+    # @exception: None
+    # @note: None
+    # @example: None
+    ################################################################################
 
-    # If the data is inserted, commit the changes
-    conn.commit()
+    def execute_sql_file(self, sql_file_path: str):
+        """
+        Execute the sql command from the sql file in the sqlite db
+        """
+        with open(sql_file_path, 'r') as f:
+            # read the file with utf-8 encoding
+            sql_commands = f.read()
+        self.cursor.executescript(sql_commands)
+        self.conn.commit()
+        self.__del__()
 
-    # close the connection to the database
-    conn.close()
 
-
-wts("exemple_cmd.sql","exemple_database.sqlite")
+Wts("exemple.sqlite").execute_sql_file("exemple.sql")
